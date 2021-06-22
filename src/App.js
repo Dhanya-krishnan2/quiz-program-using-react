24745ba45a9642d9adc7142e5bf8087ed20bc137
Dhanya-krishnan2 <dhanyakunhi@gmail.com>
2021-06-22 21:17:12 +0800

    added the routes and updated the browserRouter as router to get rid of the erros

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './Components/Header'
+import Home from './pages/Home/Home';
+import Quiz from './pages/Quiz/Quiz';
+import Result from './pages/Result/Result';
 
 function App() {
   return (
     <BrowserRouter>
     <div className="app" style={{backgroundImage: 'url("quiz.jpg")'}} >
-      <Header />
+      <Header /> 
+      <Switch>
+
+      <Router path='/' exact>
+
+      <Home />
+      </Router>
+      <Router path='/quiz' >
+
+      <Quiz/>
+      </Router>
+      <Router path='/result' >
+
+      <Result/>
+      </Router>
+      </Switch>
      
     </div>
     </BrowserRouter>
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import{TextField} from "@material-ui/core"
+
+const Home = () => {
+  return (
+    <div>
+      <TextField
+    id="outlined-secondary"
+    label="Name"
+    variant="outlined"
+    color="secondary"
+  />
+    </div>
+  )
+}
+
+export default Home
